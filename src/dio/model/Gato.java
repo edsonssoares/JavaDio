73808a13ed1820fd0d2c7187292da65a70cd39b1
edsonssoares <edsonssoares@gmail.com>
2022-07-17 17:23:06 -0300

    first commit

---
+++ src/dio/model/Gato.java
@@ -0,0 +1,76 @@
+package dio.model;
+
+import java.util.Objects;
+
+public class Gato {
+
+    // atributos
+    private String nome;
+    private String cor;
+    private Integer idade;
+
+    // alt + insert = generate
+
+    // constructor
+    public Gato() {
+    }
+
+    public Gato(String nome, String cor, Integer idade) {
+        this.nome = nome;
+        this.cor = cor;
+        this.idade = idade;
+    }
+
+
+    // getters e setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+
+    // equal() and hashCode()
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Gato gato = (Gato) o;
+        return Objects.equals(nome, gato.nome) && Objects.equals(cor, gato.cor) && Objects.equals(idade, gato.idade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, cor, idade);
+    }
+
+
+    // toString()
+    @Override
+    public String toString() {
+        return "Gato{" +
+                "nome='" + nome + '\'' +
+                ", cor='" + cor + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+}
